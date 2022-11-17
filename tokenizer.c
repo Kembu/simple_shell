03cea6388b366bcf7ffae370dd3ea501ab5f69f0
Kembu <allantoo05@gmail.com>
2022-11-16 20:25:36 -0800

    files+compiled_output

--- tokenizer.c
+++ tokenizer.c
@@ -1,4 +1,31 @@
+#include "shell.h"
 
+/**
+ * **strtow - splits a string into words. Repeat delimiters are ignored
+ * @str: the input string
+ * @d: the delimeter string
+ * Return: a pointer to an array of strings, or NULL on failure
+ */
+
+char **strtow(char *str, char *d)
+{
+	int i, j, k, m, numwords = 0;
+	char **s;
+
+	if (str == NULL || str[0] == 0)
+		return (NULL);
+	if (!d)
+		d = " ";
+	for (i = 0; str[i] != '\0'; i++)
+		if (!is_delim(str[i], d) && (is_delim(str[i + 1], d) || !str[i + 1]))
+			numwords++;
+	if (numwords == 0)
+		return (NULL);
+	s = malloc((1 + numwords) * sizeof(char *));
+	if (!s)
+		return (NULL);
+	for (i = 0, j = 0; j < numwords; j++)
+	{
 		while (is_delim(str[i], d))
 			i++;
 		k = 0;
@@ -19,7 +46,6 @@
 	s[j] = NULL;
 	return (s);
 }
-
 /**
  * **strtow2 - splits a string into words
  * @str: the input string
